ce40cd0a7463b8d467a9053280b913d1057dacff
Nikitha Mohithe <nikithamohite@Nikithas-MacBook-Air.local>
2026-01-02 01:46:05 +0530

    wtv

--- 6.KthSymbolInGrammar.cpp
+++ 6.KthSymbolInGrammar.cpp
@@ -7,37 +7,19 @@ using namespace std;
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        string res = "0";
-        long long result = std::pow(2, n-1)/2;
-        formString(res, 0, result);
-        if(result == 0) {
+        if(n==1 || k==1) {
             return 0;
         }
-        if(k>result) {
-            return res[k-result] - '0';
+        std::string s = std::to_string(n);
+        int length = pow(2, n-1);
+        if (k <= length/2) {
+            return kthGrammar(n-1, k);
         }
         else {
-            return res[k-1] - '0';
+            return !kthGrammar(n-1, k-length/2);
         }
-    }
-
-    void formString(string& s, long long pos, long long result) {
-        if(s.size() > result) {
-            cout << "from return " << pos << endl;
-            cout << "from return " << result << endl;
-            return;
-        }
-        if(s[pos] == '0') {
-            cout << pos << endl;
-            s.insert(pos+1, 1, '1'); 
-        } else {
-            cout << pos << endl;
-            s.insert(pos+1, 1, '0'); 
-        }
-        return formString(s, pos+2, result); 
     }
 };
-
 
 int main() {
   Solution solution;
